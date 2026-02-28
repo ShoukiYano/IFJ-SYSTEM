68904a330bd8494ac12ce45f6e98ef16fa66ff75
ShoukiYano <yanopitrombone@gmail.com>
2026-03-01 08:43:04 +0900

    middleware add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,20 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      const { pathname } = req.nextUrl;
+
+      // テナント専用ログインページは認証なしでアクセス可能にする
+      if (pathname.startsWith("/t/") && pathname.endsWith("/login")) {
+        return true;
+      }
+
+      // それ以外はトークンがあれば認証済みとする
+      return !!token;
+    },
+  },
+});
 
 export const config = {
   matcher: [
